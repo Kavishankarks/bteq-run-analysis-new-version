bf4dc5116847245e1a2a7aa377281f14c5cacdc6
Kavishankarks <57652470+Kavishankarks@users.noreply.github.com>
2023-04-13 17:42:23 +0530

    Update bteqAnalysisV2.py

--- bteqAnalysisV2.py
+++ bteqAnalysisV2.py
@@ -2,8 +2,6 @@ import pandas as pd
 import re 
 import streamlit as st
 import numpy as np
-import csv
-import base64
 # df = pd.read_csv("edw_sql_analysis - run5.csv")
 
 
@@ -42,7 +40,7 @@ class Summary :
         return (self.df["import_status"] == "Failed").sum()
     
     def buildSummary(self):
-        # st.write("Total SQL's : ",len(df))
+        st.write("Summary")
         col1, col2, col3, col4= st.columns(4)
         col1.metric("Total SQL's : ",len(df))
         col2.metric("Parser Completed",self.get_parse_completed())
@@ -122,7 +120,6 @@ class Summary :
                 col3.metric("Parser Error",parser)
                 col4.metric("SQL Import Error",sqlimport)
                 col5.metric("Metadata Error",metadata)
-
                 if list_error:
                     st.write(target_table)
             cnt += 1
@@ -139,7 +136,8 @@ class Summary :
         for i in df["jcl"]:
             if i  not in unique_jcls and type(i) == str:
                 unique_jcls[i] = 1
-        st.write("Total unique jcl :",len(unique_jcls))
+        st.write(unique_jcls.keys())
+        st.write(len(unique_jcls))
         dff = df[df["jcl"] != None]
         dff.fillna("NA",inplace=True)
         fully_completed =0 
@@ -160,7 +158,6 @@ class Summary :
                 col3.metric("Parser Error",parser)
                 col4.metric("SQL Import Error",sqlimport)
                 col5.metric("Metadata Error",metadata)
-
                 if list_error:
                     st.write(target_jcl)
             cnt += 1
@@ -179,7 +176,7 @@ class Summary :
             metadata_error = df[df["metadata_build_status"]=="failed"]
             st.write(metadata_error)
    
-class PreProcessData :
+class preProcessData :
     def __init__(self,df) -> None:
         self.actual_columns  = ['jcl', 'control_card', 'sql_liness', 'sql_number', 'query_type',
        'parse_status', 'table_mode', 'target_table', 'dependent_tables',
@@ -209,17 +206,7 @@ class PreProcessData :
                 self.df = self.df.rename(columns = dict(zip(self.current_columns, self.actual_columns_next)))
             except :
                 st.exception("Column count mismatch")
-        if("file_name" in self.df.columns):
-            self.df = self.split_file_name()
         return self.df
-    
-    def split_file_name(self) :
-        # extract the last two files and create new columns
-        self.df['jcl'] = df['file_path'].str.extract(r'\/(\w+)\.txt$', expand=False)
-        self.df['control_card'] = df['file_path'].str.extract(r'\/(\w+)\.txt$', expand=False)
-
-        # drop the file_path column
-        self.df.drop('file_path', axis=1, inplace=True)
     
     def replace_nan_with_na(self):
         self.df.fillna("NA",inplace=True)
@@ -245,78 +232,46 @@ uploaded_file = st.file_uploader("Choose a file")
 
 if select_screen == "BTEQ Run Analysis" :
     if uploaded_file is not None:
-        error_rows =[]
-        try :
-            df = pd.read_csv(uploaded_file)
-            # df = pd.read_csv(uploaded_file,usecols=[i for i in range(14) if i not in error_rows])
-            preprocess = PreProcessData(df)
-            df = preprocess.update_columns_names()
-            preprocess.replace_nan_with_na()
-            st.title("Run report")
-            st.write(df)
-            summary = Summary(df)
-            summary.buildSummary()
-            summary.error_display()
-            if st.button("Get All Tables Not Found"):
-                summary.get_table_not_found()
-            summary.keyword_error()
-            summary.search_target_table()
-            s = st.checkbox("List errors")
-            if st.button("Get table analysis") :
-                summary.get_table_completed(s)
-            if st.button("Get JCL analysis") :
-                summary.get_jcl_completed(s)
-            uploaded_jira_mapping = st.file_uploader("Chose Jira Mapping ")
-            if uploaded_jira_mapping is not None :
-                    df_jira = pd.read_csv(uploaded_jira_mapping)
-                    # df_jira.columns = df_jira.columns.str.lower().str.replace(' ', '_')
-                    jiramap = JiraMapping(df_jira)
-                    df_jira = jiramap.update_column_names()
-                    df_jira["error"] = df_jira["error"].str.replace('""',"")
-                    dff = df[df["target_table"] != None]
-                    dff.fillna("NA",inplace=True)
-                    dff["error"] = dff["error"].str.replace('""',"")
-                    cnt =0
-                    for i in df_jira["error"] :
-                        cnt += 1
-                        try :
-                            error_df = dff[dff["error"] == i]
-                            if(not error_df.empty):
-                                st.write(df_jira["jira_id"][cnt])
-                                st.write(error_df)
-                        except Exception as e :
-                            st.exception(f'unknown exception {e}')  
-            # st.write("sd")
-        except Exception as e:
-            st.exception(f"File has more number of field than expected {e} ")
-            error_rows = []
-            try:
-                with open(uploaded_file.name, 'r') as infile:
-                    reader = csv.reader(infile)
-                    # writer = csv.writer(outfile)
-                    header = next(reader)
-                    # st.write(type(first_row))
-                    # writer.writerow(header)
-                    df_rows = []
-                    length = len(header)
-                    for i,row in enumerate(reader):
-                        if(len(row) == length) :
-                            df_rows.append(row)
-                        else :
-                            st.write("skipping row number : ", i + 2 )
-                            st.write("row ",row)
-                df_processed_file  = pd.DataFrame(df_rows, columns=header)
-                csv = df_processed_file.to_csv(index=False)
-                b64 = base64.b64encode(csv.encode()).decode()
-                st.markdown('### Download CSV file')
-                href = f'<a href="data:file/csv;base64,{b64}" download="processed_file.csv">Download Processed CSV file</a>'
-                st.markdown(href, unsafe_allow_html=True)
-                # st.write(pd.read_csv("processed_run_file.csv"))
-                st.header(":green[File  is processed to usable format]")
-                st.header(":green[Please download and  reupload]")
-            except csv.Error as e:
-                st.write(f'Error parsing {uploaded_file}: {e}')
-                
+        df = pd.read_csv(uploaded_file)
+        preprocess = preProcessData(df)
+        df = preprocess.update_columns_names()
+        preprocess.replace_nan_with_na()
+        st.title("Run report")
+        st.write(df)
+        summary = Summary(df)
+        summary.buildSummary()
+        summary.error_display()
+        if st.button("Get All Tables Not Found"):
+            summary.get_table_not_found()
+        summary.keyword_error()
+        summary.search_target_table()
+        s = st.checkbox("List errors")
+        if st.button("Get table analysis") :
+            summary.get_table_completed(s)
+        if st.button("Get JCL analysis") :
+            summary.get_jcl_completed(s)
+        uploaded_jira_mapping = st.file_uploader("Chose Jira Mapping ")
+        if uploaded_jira_mapping is not None :
+                df_jira = pd.read_csv(uploaded_jira_mapping)
+                # df_jira.columns = df_jira.columns.str.lower().str.replace(' ', '_')
+                jiramap = JiraMapping(df_jira)
+                df_jira = jiramap.update_column_names()
+                df_jira["error"] = df_jira["error"].str.replace('""',"")
+                dff = df[df["target_table"] != None]
+                dff.fillna("NA",inplace=True)
+                dff["error"] = dff["error"].str.replace('""',"")
+                cnt =0
+                for i in df_jira["error"] :
+                    cnt += 1
+                    try :
+                        error_df = dff[dff["error"] == i]
+                        if(not error_df.empty):
+                            st.write(df_jira["jira_id"][cnt])
+                            st.write(error_df)
+                    except Exception as e :
+                        st.exception(f'unknown exception {e}') 
+        # st.write("sd")
+
 
 else :
     st.write("Compare two Bteq")
@@ -326,11 +281,11 @@ else :
         df2 = pd.read_csv(uploaded_file_second)
 
         #preprocess both files
-        preprocess_df1 = PreProcessData(df1)
+        preprocess_df1 = preProcessData(df1)
         df1 = preprocess_df1.updateColumnsNames()
         preprocess_df1.replace_nan_with_na()
         
-        preprocess_df2 = PreProcessData(df2)
+        preprocess_df2 = preProcessData(df2)
         df2 = preprocess_df2.updateColumnsNames()
         preprocess_df2.replace_nan_with_na()
 
